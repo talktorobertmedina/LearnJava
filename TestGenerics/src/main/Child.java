8d9dc6e494466c8614cd4130ff3b22427597c885
Robert Medina <talktorobertmedina@gmail.com>
2023-08-07 11:57:04 -0400

    sync

---
+++ TestGenerics/src/main/Child.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Child extends Human {
+	public Child(String name) {
+		name = name;
+	}
+}
---
+++ TestGenerics/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		Parent jack = new Parent(new ArrayList<Child> {new Child("Jack Jr.")});
+	}
+
+}
